ab14923a3b7851a0c9f86c88e3b5cbcf03fb1e1b
Scott Morey <scottrmorey@gmail.com>
2024-05-28 22:03:02 -0400

    2024.05.28 Before Bootstrap icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AngularExampleComponent } from './angular-example/angular-example.component';
 import { HomeComponent } from './home/home.component';
 import { InstructorsComponent } from './instructors/instructors.component';
+import { Test1Component } from './test1/test1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AngularExampleComponent,
     HomeComponent,
-    InstructorsComponent
+    InstructorsComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule,
